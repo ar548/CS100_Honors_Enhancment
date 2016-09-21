b8b73a7566522a9e9e9c687e8441bb61f03a61c4
ar548 <ar548@njit.edu>
2016-09-21 09:39:54 -0400

    Added table Requesting

--- MySQLTableEditor.py
+++ MySQLTableEditor.py
@@ -3,7 +3,58 @@ import mysql.connector
 # import paramiko
 
 def callRequest():
-    print('request')
+    print('requesting table')
+
+    query = 'SHOW COLUMNS FROM ' + tableRequested.get() + ';'
+
+    table = list()
+    tableRow = list()
+    q = cur.execute(query)
+    r = cur.rowcount
+    while True:
+        row = cur.fetchone()
+        if not row:
+            print('ending SHOW COLUMNS')
+            break
+        for i in row:
+            tableRow.append(str(i))
+            break
+    table.append(tableRow)
+    tableRow = list()
+
+    query = 'SELECT * FROM ' + tableRequested.get() + ';'
+
+    q = cur.execute(query)
+    r = cur.rowcount
+    while True:
+        row = cur.fetchone()
+        if not row:
+            print('ending SELECT')
+            break
+        for i in row:
+            # print(i)
+            tableRow.append(str(i))
+        table.append(tableRow)
+        tableRow = list()
+        # print("\n")
+
+    for i in table:
+        print(i)
+
+    numCols = len(table[0])
+    numRows = len(table)
+
+    tab = []
+    c = [None] * numCols
+    for i in range(numRows):
+        tab.append(c)
+
+    for i in range(numRows):
+        for j in range(numCols):
+            tab[i][j] = Entry(mainTableFrame)
+            tab[i][j].grid(row=i, column=j, padx=5, pady=5)
+            tab[i][j].insert(0, table[i][j])
+
 
 def callUpdate():
     print('update')
@@ -34,28 +85,6 @@ cur = connection.cursor()
 #     'FROM Students '
 #     'WHERE Gender=\'Male\';'
 # )
-query='SELECT * FROM Students;'
-
-q = cur.execute(query)
-r = cur.rowcount
-
-table = list()
-tableRow = list()
-while True:
-    row = cur.fetchone()
-    if not row:
-        print('ending')
-        break
-    for i in row:
-        # print(i)
-        tableRow.append(str(i))
-    table.append(tableRow)
-    tableRow = list()
-    # print("\n")
-
-for i in table:
-    print(i)
-
 
 root = Tk()
 labelTitle = Label(root, text="MySQL Table Editor")
@@ -68,26 +97,17 @@ buttonGet.grid(row = 0, column = 0)
 buttonUpdate = Button(buttonFrame, text = 'Update Table', command = callUpdate)
 buttonUpdate.grid(row = 0, column = 1)
 
+tableRequested = StringVar(root)
+tableRequested.set('Students')
+entryFrame = Frame(root)
+entryFrame.grid(row = 2, column = 0)
+labelRequest = Label(entryFrame, text = 'Table Requested')
+labelRequest.grid(row = 0, column = 0)
+entryRequest = Entry(entryFrame, textvariable = tableRequested)
+entryRequest.grid( row = 0, column = 1)
+
 mainTableFrame = Frame(root)
-mainTableFrame.grid(row=2, column=0, padx=10, pady=10)
-
-numCols = len(table[0])
-numRows = len(table)
-
-tab=[]
-c = [None]*numCols
-for i in range(numRows):
-    tab.append(c)
-
-
-for i in range(numRows):
-    for j in range(numCols):
-        tab[i][j] = Entry(mainTableFrame)
-        tab[i][j].grid(row=i, column=j, padx=5, pady=5)
-        tab[i][j].insert(0, table[i][j])
-        # tab[i][j].setvar(table[i][j])
-        # e = Entry(mainTableFrame)
-        # e.
+mainTableFrame.grid(row=3, column=0, padx=10, pady=10)
 
 mainloop()
 
